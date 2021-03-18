fa9e576b18f0d492bcfc8d7b346ae15f9a50d355
思澈 <sichecmm@gmail.com>
2021-03-18 14:48:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 
 a = np.linspace(0,10,100)
 b = np.sin(a)
 
 plt.figure()
+t1 = time.time()
 plt.plot(a,b)
 plt.savefig('test.png')
+ptint("time cost:%s" % (t2-t1))
